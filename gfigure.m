c98d8ef6c9246643f47d22950b3fd6fc17b5df13
hikuta924 <hikuta924@gmail.com>
2014-12-12 17:59:39 +0900

    Add documentation

--- gfigure.m
+++ gfigure.m
@@ -1,5 +1,37 @@
-% set(0,'defaultfigurecreatefcn',@(varargin) gfigure('tail','nofocus',varargin{1}));
 function ret = gfigure(varargin)
+	% gfigure is...
+	% - A package to let figure windows appear automatically in grid style alignment	
+	% - A command to batch resize and organize figures in grid style
+	% Automatic alignment requires some additional settings (see "Automatic Alignment" section)
+    % 
+	% ### Examples:
+	%     gfigure               ->  Align all visible figures in grid style
+	%     gfigure 1:5           ->  Align specified figures in grid style
+	%     gfigure 400 300       ->  Resize figure to 400x300 and align (setting is remembered)
+	%     gfigure row           ->  (-r) Align row-wise ('col' or '-s' for column-wise. setting is remembered)
+	%     gfigure disp 2        ->  (-d) Show on second display (setting is remembered)
+	%     gfigure nofocus       ->  (-n) Don't give focus and only align
+	%     gfigure tail          ->  (-t) Move figures to end
+	%     gfigure size 400 300  ->  (-s) Explicitly specify figure size
+	% Composite Calls are possible:
+	%     gfigure 400 300 row 1:5 disp 2
+	%     gfigure 1 400 300 row           ->  Ambiguous call. gfigure guesses min(1,300) is the figure handle
+	%     gfigure('disp',[3 1 2],400,300,[1 3 5 7 9])
+    % 
+	% ### Default Settings
+	% Default figure size, subdisplay priority, oriantation could be changed by editing variables in gfigure.m ('edit gfigure.m').
+    % 
+	% ### Automatic Alignment
+	% By executing the code below, you can let figure windows automatically appear in grid style without any commands:
+	%     set(0,'defaultfigurecreatefcn',@(varargin) gfigure('tail','nofocus',varargin{1}));
+	% By writing this code in startup.m, figrues will appear in grid style on startup of MATLAB.
+	% (see http://www.mathworks.com/help/matlab/ref/startup.html for details on startup.m)
+	% The above code alters the 'defaultfigurecreatefcn' property of 'figure'
+	% so that the 3rd argument is always called on figure window creation (this feature is currently undocumented).
+
+	% Author:
+	% 12.12.2014, Hikaru Ikuta
+
     %======================================================================
     % Settings
     %======================================================================
